d3628da6505c7f8c97e74b0f1163d35841f82a9d
cho407 <parfume407@gmail.com>
2025-02-25 22:34:59 +0900

    Add new xcode project
    Add new xcode project

---
+++ WhisperCaptionPro/WhisperCaptionProTests/WhisperCaptionProTests.swift
@@ -0,0 +1,15 @@
+//
+//  WhisperCaptionProTests.swift
+//  WhisperCaptionProTests
+//
+//  Created by 조형구 on 2/22/25.
+//
+
+import Testing
+@testable import WhisperCaptionPro
+
+struct WhisperCaptionProTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
---
+++ WhisperCaptionPro/WhisperCaptionProUITests/WhisperCaptionProUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WhisperCaptionProUITestsLaunchTests.swift
+//  WhisperCaptionProUITests
+//
+//  Created by 조형구 on 2/22/25.
+//
+
+import XCTest
+
+final class WhisperCaptionProUITestsLaunchTests: XCTestCase {
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
